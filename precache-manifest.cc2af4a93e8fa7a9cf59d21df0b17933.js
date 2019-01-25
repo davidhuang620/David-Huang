b4aa7de976f3f10991972d37ee56a3f4f2d0e4ce
David <d12yes@gmail.com>
2019-01-24 23:44:37 -0600

    Updates

---
+++ precache-manifest.cc2af4a93e8fa7a9cf59d21df0b17933.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/davidhuang620.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "500e538f5d5e2dd0dbca",
+    "url": "/davidhuang620.github.io/static/js/runtime~main.500e538f.js"
+  },
+  {
+    "revision": "03a450bb60e61aabc804",
+    "url": "/davidhuang620.github.io/static/js/main.03a450bb.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/davidhuang620.github.io/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "03a450bb60e61aabc804",
+    "url": "/davidhuang620.github.io/static/css/main.712d1f84.chunk.css"
+  },
+  {
+    "revision": "f3c5c8bf7d72494ac7a4d7c62608e954",
+    "url": "/davidhuang620.github.io/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.47748c5130ad59497e30dc1baf81140f.js"
+  "/davidhuang620.github.io/precache-manifest.cc2af4a93e8fa7a9cf59d21df0b17933.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/davidhuang620.github.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
